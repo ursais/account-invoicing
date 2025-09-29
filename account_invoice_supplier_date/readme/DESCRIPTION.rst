f1760b7cf6447159169ccb9fa9e124f25badb210
dbrogan-osi <dbrogan@opensourceintegrators.com>
2025-09-29 22:34:02 +0000

    Fix for ticket 66768: Submodule update
    Automated changes for ticket resolution

--- account_invoice_supplier_date/readme/DESCRIPTION.rst
+++
@@ -1,2 +0,0 @@
-This module move accounting date of supplier invoice near invoice date, to be
-more visible to accounting user.
--- account_invoice_tier_validation_approver/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-
-from . import test_account_invoice_tier_validation_approver
